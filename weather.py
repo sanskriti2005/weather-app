d735055dac4b6f812a50542fb8cd74ea604e1ce0
Sanskriti <sanskritisri27@gmail.com>
2024-04-09 09:59:36 +0530

    added function to get data from the api and deserialize it

--- weather.py
+++ weather.py
@@ -19,6 +19,8 @@ base_url = "https://api.openweathermap.org/data/2.5/weather"
 @click.option('-p','--pressure', help='gets the pressure data for the entered city.')
 @click.option('-h', '--humidity', help='gets the humidity data for the entered city.')
 
+
+#BUILD THE URL
 def get_url(city):
     #api key
     api_key = get_api_key()
@@ -29,4 +31,38 @@ def get_url(city):
     #the url 
     url = (f"{base_url}?q={city_name}&appid={api_key}")
     return url
+
+#GET DATA FROM THE URL
+def get_data(url):
+    #create a request object and add a user-agent
+    req = request.Request(url, headers={'User-Agent':'Mozilla/5.0'})
+
+    #initialting the http request from the request object
+    try:
+        response = request.urlopen(req)
+    except error.HTTPError as http_error:
+        #401 Unauthorized
+        if http_error.code == 401:
+            sys.exit("Access Denied, Check your API key.")
+         #401 Not-Found
+        elif http_error.code == 404:
+            sys.exit("Can't find the data for the mentioned city.")
+        else: 
+            sys.exit(f"Something went wrong... ({http_error.code})")
+    
+    #reading data from the response
+    data = response.read()
+
+    #return deserialized json
+    try:
+        return json.loads(data)
+        
+    #unless.. there is an error 
+    except:
+         sys.exit("Couldn't read the server response")
+
+    
+
+    
+    
 
