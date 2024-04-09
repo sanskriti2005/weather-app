4ec499869fa04d9b7a4b1ce4d9c4df4b62749c96
Sanskriti <sanskritisri27@gmail.com>
2024-04-09 09:51:12 +0530

    added the function to build the url

--- weather.py
+++ weather.py
@@ -8,10 +8,25 @@ import sys
 def get_api_key():
     return os.getenv("OpenWeatherAPI")
 
+
+#BASE URL TO CREATE THE URL
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+
+
 #ARGUMENTS
 @click.command()
 @click.argument('city')
 @click.option('-p','--pressure', help='gets the pressure data for the entered city.')
 @click.option('-h', '--humidity', help='gets the humidity data for the entered city.')
 
+def get_url(city):
+    #api key
+    api_key = get_api_key()
+
+    #url encoded city name
+    city_name = parse.quote_plus(city)
+
+    #the url 
+    url = (f"{base_url}?q={city_name}&appid={api_key}")
+    return url
 
